867988c6989f503ba59790b846aba978071ce939
simon-norman <=>
2025-04-24 09:08:28 +0100

    fix

--- src/fastify/api.ts
+++ src/fastify/api.ts
@@ -29,7 +29,7 @@ type FastifyOptions<ExpectedConfig extends TProperties> = {
 	 * before you tell the server to start
 	 * Access the config with fastifyApi.config.loadedConfig
 	 */
-	config?: ConfigOpts<ExpectedConfig>;
+	config?: Exclude<ConfigOpts<ExpectedConfig>, "logger">;
 	globalLogContext?: Record<string, unknown>;
 };
 
