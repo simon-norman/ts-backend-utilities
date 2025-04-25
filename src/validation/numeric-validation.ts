e533182d0f3cf6eb4d7aacdefb1838b50cf23aed
simon-norman <=>
2025-04-25 10:27:44 +0100

    fix

--- src/validation/numeric-validation.ts
+++ src/validation/numeric-validation.ts
@@ -1,4 +1,10 @@
-import { Type as t } from "@sinclair/typebox";
+import { NumberOptions, Type as t } from "@sinclair/typebox";
 import { defaultValidation } from "./default-validation";
 
 export const defaultNumeric = t.Number(defaultValidation);
+
+export const defaultNumber = (options?: NumberOptions) => {
+	return t.Number({
+		...options,
+	});
+};
--- src/validation/string-validation.ts
+++ src/validation/string-validation.ts
@@ -1,4 +1,11 @@
-import { Type as t } from "@sinclair/typebox";
+import { StringOptions, Type as t } from "@sinclair/typebox";
 import { defaultValidation } from "./default-validation";
 
 export const defaultString = t.String(defaultValidation);
+
+export const defaultStr = (options?: StringOptions) => {
+	return t.String({
+		...defaultValidation,
+		...options,
+	});
+};
